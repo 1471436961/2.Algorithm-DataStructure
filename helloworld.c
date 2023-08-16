49906685783dd842348cc9b924d717d686ed660b
zhouyingda <zhouyingda@localhost.localdomain>
2023-08-16 21:55:54 +0800

    add helloworld.c

---
+++ helloworld.c
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: helloworld.c
+	> Author: 
+	> Mail: 
+	> Created Time: Wed 16 Aug 2023 09:53:03 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main() {
+    printf("Hello World!\n");
+    return 0;
+}
