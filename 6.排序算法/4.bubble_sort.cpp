f66e47bb8ae721302d46ff4883398a8271c11aaa
1471436961 <1471436961@qq.com>
2024-03-19 21:11:58 +0800

    update

--- 6.排序算法/4.bubble_sort.cpp
+++ 6.排序算法/4.bubble_sort.cpp
@@ -10,14 +10,14 @@
 #include "0.sort_test.h"
 
 void bubble_sort(int *arr, int l, int r) {
-    for (int i = r - 1, I = l + 1, cnt; i >= I; i--) {
-        cnt = 0;
+    for (int i = r - 1, I = l + 1, flag; i >= I; i--) {
+        flag = 0;
         for (int j = l; j < i; j++) {
             if (arr[j] <= arr[j + 1]) continue;
             swap(arr[j], arr[j + 1]);
-            cnt += 1;
+            flag = 1;
         }
-        if (cnt == 0) break;
+        if (flag == 0) break;
     }
     return ;
 } 
