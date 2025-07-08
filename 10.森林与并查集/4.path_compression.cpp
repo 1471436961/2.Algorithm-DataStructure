c4c6eef71f1240376a56a1d1162b8a0e118d3102
1471436961 <1471436961@qq.com>
2025-07-08 17:28:03 +0800

    update chapter10

---
+++ 10.森林与并查集/4.path_compression.cpp
@@ -0,0 +1,68 @@
+/*************************************************************************
+	> File Name: 4.path_compression.cpp
+	> Author: zhouyingda
+	> Mail: 
+	> Created Time: Tue 08 Jul 2025 02:46:56 PM CST
+ ************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+#define MAX_N 10000
+int fa[MAX_N + 5];
+int sz[MAX_N + 5]; // 用size会和标准库中的命名冲突
+
+void init(int n) {
+    for (int i = 0; i <= n; i++) {
+        fa[i] = i;
+        sz[i] = 1;
+    }
+    return ;
+}
+
+int find(int x) {
+    return fa[x] = (fa[x] == x ? x : find(fa[x]));
+} // 路径压缩优化
+
+int merge(int a, int b) {
+    int aa = find(a), bb = find(b);
+    if (aa == bb) return 0;
+    if (sz[aa] < sz[bb]) {
+        fa[aa] = bb;
+        sz[bb] += sz[aa];
+    } else {
+        fa[bb] = aa;
+        sz[aa] += sz[bb];
+    }
+    return 1;
+} 
+
+void output(int n) {
+    int ret = 0;
+    for (int i = 0; i <= n; i++) {
+        ret += printf("%3d", i);
+    }
+    printf("\n");
+    for (int i = 0; i < ret; i++) printf("-");
+    printf("\n");
+    for (int i = 0; i <= n; i++) {
+        printf("%3d", fa[i]);
+    }
+    printf("\n");
+    for (int i = 0; i<= n; i++) {
+        printf("%3d", sz[i]);
+    }
+    printf("\n");
+    return ;
+}
+
+int main() {
+    int n, a, b;
+    cin >> n;
+    init(n);
+    while (cin >> a >> b) {
+        printf("merge %d with %d : %d\n", a, b, merge(a, b));
+        output(n);
+    }
+    return 0;
+}
